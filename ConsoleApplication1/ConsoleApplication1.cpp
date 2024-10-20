64cab0dad4f09f2cefb0dad312d518b2742981aa
Бачок Микола < @DESKTOP-AHTUBL4>
2024-10-20 20:40:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath> // для sin() и log()
+
+using namespace std;
+
+int main() {
+    double x, y;
+    cout << "x: ";
+    cin >> x;
+
+    if (x < 0) {
+        y = sin(pow(x, 2)); // sin(x^2)
+    }
+    else {
+        y = log(x + 1); // ln(x + 1)
+    }
+
+    cout << "y: " << y << endl;
+
+    return 0;
+}
